dd743f98bf326f28410c35c8361c4ab9277a6c81
TheBattleDog <krishnanand7777777@gmail.com>
2021-01-14 18:43:27 +0530

    file not found algorithm improved

--- Password_Manager.cpp
+++ Password_Manager.cpp
@@ -4,9 +4,14 @@ int main()
 {
 	std::string master_password;
 	file_h::get_password(master_password);
-	pass::Master_Password(master_password);
+	if(master_password.size())
+		pass::Master_Password(master_password);
 	system("cls");
-	pass::Interface(master_password);
+	while (strf::RUN())
+	{
+		pass::Interface(master_password);
+		system("cls");
+	}
 
 	return 0;
 }
